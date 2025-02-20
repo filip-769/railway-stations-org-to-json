fc82f8cde261adadbf464f067b90c60e673ff75b
filip-769 <77580207+filip-769@users.noreply.github.com>
2025-02-20 19:04:48 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const countries = (await (await fetch("https://api.railway-stations.org/countries")).json()).map(country => country.code);
+
+const list = (await Promise.all(countries.map(async country => {
+    const countryResponse = await fetch(`https://api.railway-stations.org/photoStationsByCountry/${country}?hasPhoto=false`);
+    const { stations } = await countryResponse.json();
+
+    return stations.map(station => {
+        return {
+            lat: station.lat,
+            lon: station.lon,
+            photos: station.photos.map(photo => {
+                return {
+                    author: photo.photographer,
+                    date: new Date(photo.createdAt),
+                    imageUrl: `https://api.railway-stations.org/photos${photo.path}`,
+                    license: licenseToSpdx(photo.license),
+                    pageUrl: `https://map.railway-stations.org/station.php?countryCode=${country}&stationId=${station.id}`,
+                    thumbUrl: `https://api.railway-stations.org/photos${photo.path}?width=500`,
+                    source: "railway-stations.org"
+                }
+            })
+        }
+    })
+}))).flat()
+
+Deno.writeTextFileSync("list.json", list);
+
+function licenseToSpdx(code) {
+    if(code === "CC0_10") return "CC0-1.0";
+    if(code === "CC_BY_30") return "CC-BY-3.0";
+    if(code === "CC_BY_SA_40") return "CC-BY-SA-4.0";
+    if(code === "CC_BY_NC_40_INT") return "CC-BY-NC-4.0";
+    if(code === "CC_BY_NC_SA_30_DE") return "CC-BY-NC-SA-3.0-DE";
+}
